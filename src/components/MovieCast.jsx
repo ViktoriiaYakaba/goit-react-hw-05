d513c58d4572c9e103b337e50edb03f7d19556ee
ViktoriiaYakaba <yakaba@bk.ru>
2024-03-20 17:14:21 +0100

    update

--- src/components/MovieCast.jsx
+++ src/components/MovieCast.jsx
@@ -6,31 +6,52 @@ import { useParams } from 'react-router-dom';
 const MovieCast = () => {
     const { movieId } = useParams();
     const [cast, setCast] = useState([]);
+     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchCast = async () => {
-            const castData = await fetchMovieCast(movieId);
-            setCast(castData);
+            try {
+                const castData = await fetchMovieCast(movieId);
+                setCast(castData);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching movie cast:", error);
+                setError("An error occurred while fetching movie cast. Please try again later.");
+                setLoading(false);
+            }
         };
         fetchCast();
-    }, [movieId]); 
+    }, [movieId]);
 
-    return (
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+     return (
         <div>
             <h2>Movie Cast</h2>
-            <ul>
-                {cast.map(actor => (
-                    <li key={actor.id}>
-                        {actor.profile_path ? (
-                            <img src={`https://image.tmdb.org/t/p/w500/${actor.profile_path}`} alt={actor.name} width="130" />
-                        ) : (
-                            <div>No Image Available</div>
-                        )}
-                        <p>{actor.name}</p>
-                        <p>Character: {actor.character}</p>
-                    </li>
-                ))}
-            </ul>
+            {cast.length > 0 ? (
+                <ul>
+                    {cast.map(actor => (
+                        <li key={actor.id}>
+                            {actor.profile_path ? (
+                                <img src={`https://image.tmdb.org/t/p/w500/${actor.profile_path}`} alt={actor.name} width="130" />
+                            ) : (
+                                <div>No Image Available</div>
+                            )}
+                            <p>{actor.name}</p>
+                            <p>Character: {actor.character}</p>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No cast information available.</p>
+            )}
         </div>
     );
 }
--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -1,62 +1,62 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { fetchMovieDetails } from "../components/MovieApi";
-import { useParams, Link, Outlet } from 'react-router-dom';
+import { useParams, NavLink, Outlet, useLocation } from 'react-router-dom';
 import css from './MovieDetailsPage.module.css';
 import { MdArrowBack } from "react-icons/md";
-
 
 const MovieDetails = () => {
     const { movieId } = useParams();
     const [movie, setMovie] = useState(null);
- 
+    const location = useLocation();
+    const backLinkRef = useRef(location.state);
+
     useEffect(() => {
         const detailsMovie = async () => {
-                const details = await fetchMovieDetails(movieId);
-                setMovie(details);
-            
+            const details = await fetchMovieDetails(movieId);
+            setMovie(details);
         };
         detailsMovie();
     }, [movieId]); 
 
- const formatScore = (score) => {
+    const formatScore = (score) => {
         return `${score}%`;
     };
 
-     return (
-         <div >
-            <Link to='/' className={css.btnBack}><MdArrowBack />Go back</Link>
+    return (
+        <div >
+            <NavLink to={backLinkRef.current}  className={css.btnBack}><MdArrowBack />Go back</NavLink>
             {movie && (
                 <div className={css.container}>
                     <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} width='250'/>
                     <div className={css.list}>
-                         <h2 className={css.title}>{movie.title}</h2>
+                        <h2 className={css.title}>{movie.title}</h2>
                         <p className={css.score}>User Score: {formatScore(movie.vote_average)}</p>
                         <p className={css.overview}>{movie.overview}</p>
                         <p className={css.genres}><span>Genres</span> {movie.genres.map(genre => genre.name).join(', ')}</p>
-                     </div>
-                 </div>
-             )}
+                    </div>
+                </div>
+            )}
              
-           {movie && (
+            {movie && (
                 <div className={css.info}>
-                    <h4 className={css.titleInfo}>Aditional information</h4>
+                    <h4 className={css.titleInfo}>Additional information</h4>
                     <ul className={css.listInfo}>
-                        
                         <li>
-                            <Link to={`/movies/${movieId}/cast`} className={css.itemInfo}>Movie Cast</Link>
-                         </li>
-                         <li>
-                             <Link to={`/movies/${movieId}/reviews`} className={css.itemInfo}>Movie Reviews</Link>
-                         </li>
-                     </ul>
-                     <Outlet />
+                            <NavLink to={`/movies/${movieId}/cast`} className={css.itemInfo}>Movie Cast</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`/movies/${movieId}/reviews`} className={css.itemInfo}>Movie Reviews</NavLink>
+                        </li>
+                    </ul>
+                    <Outlet />
                 </div>
             )}
         </div>
-  
-)};
+    );
+};
 
 export default MovieDetails;
+
 
 
 
